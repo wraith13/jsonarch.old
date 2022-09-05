1e999de27d2c7a9977b8bb18d55749bafd6f997e
道化師 <wraith13@hotmail.co.jp>
2022-09-05 09:11:38 +0900

    🛠 実装 ( WIP )

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 module PlasticJson
 {
     const isConsoleMode = (typeof window !== 'undefined');
-    export const compile = () =>
+    export type JsonableValue = null | boolean | number | string;
+    export interface JsonableObject
     {
-
+        [key: string]: undefined | Jsonable;
+    }
+    export type Jsonable = JsonableValue | Jsonable[] | JsonableObject;
+    export type JsonablePartial<Target> = { [key in keyof Target]?: Target[key] } & JsonableObject;
+    export const jsonStringify = <T extends Jsonable>(source: T, replacer?: (this: any, key: string, value: any) => any, space?: string | number) => JSON.stringify(source, replacer, space);
+    export const compile = (template: Jsonable, _params?:Jsonable, _settings?: Jsonable):Jsonable =>
+    {
+        return template;
     };
 }
