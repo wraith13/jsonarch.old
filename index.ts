f8470cb75cff259aa8df16cae4aee253a8657ab9
道化師 <wraith13@hotmail.co.jp>
2022-09-06 00:16:54 +0900

    🛠 実装 ( WIP )

--- index.ts
+++ index.ts
@@ -9,8 +9,29 @@ module PlasticJson
     export type Jsonable = JsonableValue | Jsonable[] | JsonableObject;
     export type JsonablePartial<Target> = { [key in keyof Target]?: Target[key] } & JsonableObject;
     export const jsonStringify = <T extends Jsonable>(source: T, replacer?: (this: any, key: string, value: any) => any, space?: string | number) => JSON.stringify(source, replacer, space);
-    export const compile = (template: Jsonable, _params?:Jsonable, _settings?: Jsonable):Jsonable =>
+    interface Settings extends JsonableObject
     {
-        return template;
+        language?: string;
+        indent?: "minify" | "tab" | number;
+        timeout?: number;
+        profile?: boolean;
+        trace?: "stdout" | "stderr" | boolean;
+        originMap?: boolean;
+        callGraph?: boolean;
+    }
+    interface Results extends JsonableObject
+    {
+        result: Jsonable;
+        profile?: any;
+        trace?: any;
+        callGraph?: any;
+    }
+    export const compile = (template: Jsonable, _params?:Jsonable, _settings?: Settings):Results =>
+    {
+        const result =
+        {
+            result: template,
+        };
+        return result;
     };
 }
